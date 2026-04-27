f6566272458b87651cad1260e319e11d49f86963
pprando <pedro.schlens@gmail.com>
2026-04-27 09:30:20 -0300

    feat: add Toast component for displaying feedback messages

---
+++ app/components/feedback/Toast.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { ToastType } from '~/types/feedback'
+
+const props = defineProps<{
+  show: boolean
+  message: string
+  type: ToastType
+}>()
+</script>
+
+<template>
+  <div
+    v-if="props.show"
+    class="fixed bottom-5 left-1/2 -translate-x-1/2 px-4 py-3 rounded-xl text-white text-sm"
+    :class="props.type === 'success' ? 'bg-green-600' : 'bg-red-600'"
+  >
+    {{ props.message }}
+  </div>
+</template>
